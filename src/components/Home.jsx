d08b5df20293bf8a0efdb9c4cf8fca0358c5dbd8
askhit <askhit5589@gmail.com>
2024-09-25 14:42:29 +0530

    dg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,6 +14,10 @@ const Home = () => {
     { url: 'https://www.doleep.com/wp-content/webpc-passthru.php?src=https://www.doleep.com/wp-content/uploads/2023/06/explainer.jpg&nocache=1', link: 'https://drive.google.com/drive/folders/1JLf-_akevh9jgBpeQH4g1p-yFF8-F4XT?usp=sharing' },
     { url: 'https://www.griffinpictures.in/uploads/images/de8ffbcc-6fd6-4aa3-bb42-dfe2eb2718f1.jpg', link: 'https://drive.google.com/drive/folders/1wtGxBsQ7tuJERXQI7_MNIY88bc3xoy-g?usp=drive_link' },
     { url: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/label-design-template-03d767a032bed59d04b7ac7339481eff_screen.jpg?ts=1647332442', link: 'https://drive.google.com/drive/folders/1i-wAMAYcKovHgd7XLClbCIU26z0r9y-y?usp=sharing' },
+    { url: 'https://www.myhoardings.com/ads/wp-content/uploads/2021/02/Digital-Advertising-18.jpg' , link: ''},
+    { url: 'https://www.clippingpathcampus.com/wp-content/uploads/2022/05/Product-Photography-Props.jpg', link: 'https://drive.google.com/drive/folders/1pCSPQCut_nWb2Voazhlkkq_mYWAgyGsB?usp=sharing'},
+    { url: 'https://i0.wp.com/zoomdesigns.in/wp-content/uploads/edd/2023/12/wedding-invitation-8.jpg?resize=150150&ssl=1', link: 'https://drive.google.com/drive/folders/1F0ewzELuxw159UKEiA0Rz1-8JaxoH1Ot?usp=sharing'},
+    { url: 'https://buzzflick.com/wp-content/uploads/2022/04/3d-animation-video-rate-and-cost.png', link: 'https://drive.google.com/drive/folders/18Y8Sklh3dRV9feug-CqoFBd4oIb10L8R?usp=sharing'},
   ];
 
   return (
